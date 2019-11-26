315c54bc8293e9f57ecb181d5b1b0844d0de079c
Spyridakis Christos <spyridakischristos@gmail.com>
2019-11-26 18:41:56 +0200

    Working on login page

--- web/src/index.php
+++ web/src/index.php
@@ -1,5 +1,4 @@
 <?php
-
 // Create connection
 // // $conn = new mysql($servername, $username, $password, $dbname);
 
@@ -28,3 +27,36 @@
 
 // $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login page</title>
+  <link rel="stylesheet" href="css/login.css" type="text/css">
+  <script type="text/javascript" src="js/login.js"></script>
+</head>
+<body>
+<div class="login-page">
+    <div class="form">
+    <div class='login-form-text'><h2>Welcome to Students System</h2></div>
+    <form class="register-form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+    </form>
+    <form class="login-form">
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+      <p class="message">Not registered? <a href="#">Create an account</a></p>
+    </form>
+  </div>
+</div>
+</body>
+</html>
+
