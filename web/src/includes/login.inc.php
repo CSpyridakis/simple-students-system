1711112deae32c51bad8a436e206a00dd317161b
Spyridakis Christos <spyridakischristos@gmail.com>
2019-11-27 00:14:13 +0200

    Register extra checks

--- web/src/includes/login.inc.php
+++ web/src/includes/login.inc.php
@@ -26,3 +26,34 @@
 // // }
 
 // $conn->close();
+
+
+// -------------------------------------------
+// DATABASE TABLES
+// 
+// Create table Teachers (
+//     ID VARCHAR(255) NOT NULL,
+//     NAME VARCHAR(255) NOT NULL,
+//     SURNAME VARCHAR(255) NOT NULL,
+//     USERNAME VARCHAR(255) NOT NULL,
+//     PASSWORD VARCHAR(255) NOT NULL,
+//     EMAIL VARCHAR(255) NOT NULL UNIQUE,
+//     PRIMARY KEY(ID)
+// );
+// Create table Students (
+//     ID VARCHAR(255) NOT NULL UNIQUE,
+//     NAME VARCHAR(255) NOT NULL,
+//     SURNAME VARCHAR(255) NOT NULL,
+//     FATHERNAME VARCHAR(255) NOT NULL,
+//     GRADE FLOAT NOT NULL,
+//     MOBILENUMBER VARCHAR(255) NOT NULL,
+//     Birthday DATE NOT NULL,
+//     PRIMARY KEY(ID)
+// );
+// INSERT INTO `Teachers` (`ID`, `NAME`, `SURNAME`, `USERNAME`, `PASSWORD`, `EMAIL`) VALUES ('JDoe', 'John', 'Doe', 'JDoe_1', 'johnPass1', 'john-doe@email.com');
+// INSERT INTO `Teachers` (`ID`, `NAME`, `SURNAME`, `USERNAME`, `PASSWORD`, `EMAIL`) VALUES ('JSmith', 'Joe', 'Smith', 'JSmith_1', 'joePass1', 'joe-smith@email.com');
+// INSERT INTO `Students` (`ID`, `NAME`, `SURNAME`, `FATHERNAME`, `GRADE`, `MOBILENUMBER`, `Birthday`) VALUES ('AEinstein', 'Albert', 'Einstein', 'Hermann', '19.05', '(+18)-791-804-1955', '1879-03-14');
+// INSERT INTO `Students` (`ID`, `NAME`, `SURNAME`, `FATHERNAME`, `GRADE`, `MOBILENUMBER`, `Birthday`) VALUES ('CShannon', 'Claude', 'Shannon', 'Elwood', '19.37', '(+19)-162-402-2001', '1916-04-30');
+// INSERT INTO `Students` (`ID`, `NAME`, `SURNAME`, `FATHERNAME`, `GRADE`, `MOBILENUMBER`, `Birthday`) VALUES ('ATuring', 'Alan', 'Turing', 'Matheson', '19.31', '(+19)-120-706-1954', '1912-06-23');
+
+
--- web/src/signup.php
+++ web/src/signup.php
@@ -6,20 +6,21 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Sign up</title>
   <link rel="stylesheet" href="css/login_register.css" type="text/css">
+  <script src="js/register.js"></script> 
 </head>
 <body>
 <div class="login_register-page">
     <div class="form">
     <div class='login_register-form-text'><h2>Welcome to Students Management System</h2></div>
-    <form class="register-form" action="includes/register.inc.php" method="post">
-      <input type="text" name="register-id" maxlength="100" placeholder="ID*" required pattern="[A-Za-z0-9]+" title="Could contain only latin characters and numbers."/>
-      <input type="text" name="register-name" maxlength="100" placeholder="Name*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
-      <input type="text" name="register-surname" maxlength="100" placeholder="Surname*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
-      <input type="text" name="register-username" maxlength="100" placeholder="Username*" required pattern="[A-Za-z0-9]+" title="Could contain only latin characters, numbers and underscore."/>
-      <input type="email" name="register-email1" maxlength="100" placeholder="Email*" title="Please enter an acceptable email. E.g: joe-smith_2@email.com" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
-      <input type="email" name="register-email2" maxlength="100" placeholder="Email (again)*" title="Please enter an acceptable email. E.g: joe-smith_2@email.com" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
-      <input type="password" name="register-pwd1" maxlength="100" placeholder="Password*" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
-      <input type="password" name="register-pwd2" maxlength="100" placeholder="Password (again)*" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
+    <form name="register-form" class="register-form" action="includes/register.inc.php" onsubmit="return validateForm()" method="post">
+      <input type="text" name="register-id" maxlength="255" placeholder="ID*" required pattern="[A-Za-z0-9]+" title="Could contain only latin characters and numbers."/>
+      <input type="text" name="register-name" maxlength="255" placeholder="Name*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
+      <input type="text" name="register-surname" maxlength="255" placeholder="Surname*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
+      <input type="text" name="register-username" maxlength="255" placeholder="Username*" required pattern="[A-Za-z0-9]+" title="Could contain only latin characters, numbers and underscore."/>
+      <input type="email" name="register-email1" maxlength="255" placeholder="Email*" title="Please enter an acceptable email. E.g: joe-smith_2@email.com" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
+      <input type="email" name="register-email2" maxlength="255" placeholder="Email (again)*" title="Please enter an acceptable email. E.g: joe-smith_2@email.com" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
+      <input type="password" name="register-pwd1" maxlength="255" placeholder="Password*" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
+      <input type="password" name="register-pwd2" maxlength="255" placeholder="Password (again)*" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
       <button type="submit" name="register-submit">SIGN UP</button>
       <p class="message">Already registered? <a href="index.php">Sign In</a></p>
     </form>
