b12bfade8a8e89d4d21f2d907f97ac3684943244
Spyridakis Christos <spyridakischristos@gmail.com>
2019-11-27 10:17:57 +0200

    Add new student start

--- web/src/AddStudent.php
+++ web/src/AddStudent.php
@@ -7,9 +7,32 @@
     </style>
 
     <main>
-        <div class="main-content">
-            
-        </div>
+        <!-- <div class="main-content"> -->
+            <?php
+                if($_GET['error'] == "sqlerror"){
+                echo "<script>alert('There is something wrong with the database');</script>";
+                }
+                else if($_GET['error'] == "userexists"){
+                echo "<script>alert('ID, Username or Email already exists!');</script>";
+                }
+            ?>
+            <div class="add-user-page">
+                <div class="form">
+                <div class='add-user-form-text'><h2>Add new Student</h2></div>
+                <form name="add-user-form" class="add-user-form" onsubmit="return validateForm()" action="includes/register.inc.php"  method="post">
+                <input type="text" name="add-user-id" maxlength="255" placeholder="ID*" required pattern="[A-Za-z0-9]+" title="Could contain only latin characters and numbers."/>
+                <input type="text" name="add-user-name" maxlength="255" placeholder="Name*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
+                <input type="text" name="add-user-surname" maxlength="255" placeholder="Surname*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
+                <input type="text" name="add-user-username" maxlength="255" placeholder="Username*" required pattern="[A-Za-z0-9_]+" title="Could contain only latin characters, numbers and underscore."/>
+                <input type="email" name="add-user-email1" maxlength="255" placeholder="Email*" title="Please enter an acceptable email. E.g: joe-smith_2@email.com" required pattern="[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
+                <input type="email" name="add-user-email2" maxlength="255" placeholder="Email (again)*" title="Please enter an acceptable email. E.g: joe-smith_2@email.com" required pattern="[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
+                <input type="password" name="add-user-pwd1" maxlength="50" placeholder="Password*" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
+                <input type="password" name="add-user-pwd2" maxlength="50" placeholder="Password (again)*" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
+                <button type="submit" name="add-user-submit">ADD</button>
+                </form>
+            </div>
+            </div>
+        <!-- </div> -->
     </main>
 
 <?php
--- web/src/includes/header.inc.php
+++ web/src/includes/header.inc.php
@@ -5,6 +5,7 @@
         header("Location: ../index.php");
         exit();
     }
+    $username = $_SESSION["Username"];
 ?>
 
 <!DOCTYPE html>
@@ -15,26 +16,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php echo "<title>".substr(basename($_SERVER['PHP_SELF']),0,-4)."</title>"; ?>
     <link rel="stylesheet" href="../css/header.css" type="text/css">
-    <!-- <link rel="stylesheet" href="../css/main.css" type="text/css"> -->
+    <link rel="stylesheet" href="../css/main.css" type="text/css">
 </head>
 <body>
     <header>
         <nav class ="menubar">
             <ul>
-                <li><div class="left"><a href="Teachers.php">Home</a></div></li>
-                <li><div class="left"><a href="AddStudent.php">Add</a></div></li>
-                <li><div class="left"><a href="EditStudent.php">Edit</a></div></li>
-                <li><div class="left"><a href="DeleteStudent.php">Delete</a></div></li>
-                <li>
-                    <div class="left wrap search-div">
-                        <form class="search-form search" action="">
-                            <input type="text" name="searchTerm" placeholder="Search">
-                            <button type="submit" class="searchButton">Search</button>
-                        </form>
-                    </div>
-                </li>
-                <li><div class="nav-user right">USERNAME</div></li>
-                <li><div class="nav-logout right"><a href="/includes/logout.inc.php">Logout</a></div></li>
+                <li><div class="nav-item left"><a href="Teachers.php">Home</a></div></li>
+                <li><div class="nav-item left"><a href="AddStudent.php">Add</a></div></li>
+                <li><div class="nav-item left"><a href="EditStudent.php">Edit</a></div></li>
+                <li><div class="nav-item left"><a href="DeleteStudent.php">Delete</a></div></li>
+                <li><div class="nav-item left"><a href="SearchStudent.php">Search</a></div></li>
+                <?php echo '<li><div class="nav-item nav-user right">'.$username.'</div></li>' ; ?>
+                <li><div class="nav-item nav-logout right"><a href="/includes/logout.inc.php">Logout</a></div></li>
             </ul>
         </nav>
     </header>
+    
