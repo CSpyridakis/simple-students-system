8bc660af4ecee777cff173ccea3f00070e220fdc
Spyridakis Christos <spyridakischristos@gmail.com>
2019-11-27 15:37:05 +0200

    add new user page completed

--- web/src/AddStudent.php
+++ web/src/AddStudent.php
@@ -5,34 +5,41 @@
     <style>
         <?php include 'css/add_students.css'; ?>
     </style>
-
+    <script>
+        <?php include 'css/add_students.css'; ?>
+    </script>
     <main>
         <!-- <div class="main-content"> -->
             <?php
-                if($_GET['error'] == "sqlerror"){
-                echo "<script>alert('There is something wrong with the database');</script>";
+                if(isset($_GET['error'])){
+                    if($_GET['error'] == "sqlerror"){
+                    echo "<script>alert('There is something wrong with the database');</script>";
+                    }
+                    else if($_GET['error'] == "userexists"){
+                    echo "<script>alert('Username already exists');</script>";
+                    }
                 }
-                else if($_GET['error'] == "userexists"){
-                echo "<script>alert('ID, Username or Email already exists!');</script>";
+                else if (isset($_GET['add-student'])){
+                    if($_GET['add-student'] == "true"){
+                    echo "<script>alert('User Created!');</script>";
+                    }
                 }
             ?>
-            <div class="add-user-page">
+            <div class="add-student-page">
                 <div class="form">
-                <div class='add-user-form-text'><h2>Add new Student</h2></div>
-                <form name="add-user-form" class="add-user-form" onsubmit="return validateForm()" action="includes/register.inc.php"  method="post">
-                <input type="text" name="add-user-id" maxlength="255" placeholder="ID*" required pattern="[A-Za-z0-9]+" title="Could contain only latin characters and numbers."/>
-                <input type="text" name="add-user-name" maxlength="255" placeholder="Name*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
-                <input type="text" name="add-user-surname" maxlength="255" placeholder="Surname*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
-                <input type="text" name="add-user-username" maxlength="255" placeholder="Username*" required pattern="[A-Za-z0-9_]+" title="Could contain only latin characters, numbers and underscore."/>
-                <input type="email" name="add-user-email1" maxlength="255" placeholder="Email*" title="Please enter an acceptable email. E.g: joe-smith_2@email.com" required pattern="[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
-                <input type="email" name="add-user-email2" maxlength="255" placeholder="Email (again)*" title="Please enter an acceptable email. E.g: joe-smith_2@email.com" required pattern="[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
-                <input type="password" name="add-user-pwd1" maxlength="50" placeholder="Password*" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
-                <input type="password" name="add-user-pwd2" maxlength="50" placeholder="Password (again)*" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
-                <button type="submit" name="add-user-submit">ADD</button>
+                <div class='add-student-form-text'><h2>Add new Student</h2></div>
+                <form name="add-student-form" class="add-student-form" action="includes/addstudent.inc.php"  method="post">
+                <input type="text" name="add-student-id" maxlength="255" placeholder="ID*" required pattern="[A-Za-z0-9]+" title="Could contain only latin characters and numbers."/>
+                <input type="text" name="add-student-name" maxlength="255" placeholder="Name*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
+                <input type="text" name="add-student-surname" maxlength="255" placeholder="Surname*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
+                <input type="text" name="add-student-fathername" maxlength="255" placeholder="Father's Name*" required pattern="[A-Za-z]+" title="Could contain only latin characters."/>
+                <input type="number" name="add-student-grade" placeholder="Grade*" value="" required min="0" value="20" step="0.01" title="Could be a floating point number."/>
+                <input type="text" name="add-student-mobile" maxlength="255" placeholder="Mobile*" required pattern="[+0-9\-\(\) ]{10,20}" title="Could contain only numbers, space, dash and parentesis."/>
+                <input type="date" name="add-student-birthday" placeholder="Birthday*" value="" required title="Could be date."/>
+                <button type="submit" name="add-student-submit">ADD</button>
                 </form>
             </div>
             </div>
-        <!-- </div> -->
     </main>
 
 <?php
